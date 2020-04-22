542f0e4b358f43c7807dd68c56400b163c2d4a0d
= <emma@Emmas-MacBook-Pro.local>
2020-04-21 18:03:14 -0700

    blah

--- pages/Contact/index.js
+++ pages/Contact/index.js
@@ -1,19 +1,47 @@
-import {useEffect} from 'react';  
+import {useState, useEffect} from 'react';  
 import CustomButton from '../../comps/CustomButton';
 import Header from '../../comps/Header';
 import './contact.css'; 
 
+var index = 0; 
+const headers_arr = [
+    "thanks for contacting Me!",
+    "GoodBye",
+    "let me know if you have other questions"
+]
+
 const Contact = () => {
+
+    const [header_text, setHeader] = useState("Contact me!"); 
+    const [pageright, setRight] = useState("100%");
+    // const [index, setIndex] = useState(0); 
 
     useEffect(()=>{
         setTimeout(()=>{
-            document.querySelector("#contactpage").style.right = 0;
+            setRight(0);
         }, 100); 
     }, []); 
 
-return <div id='contactpage'>
-    <Header text="Contact me!" fontSize={24} />
-    <CustomButton text="email" />
+    useEffect(() => {
+        // alert('hi! Header text has changed');
+        setRight(20);
+        setTimeout(()=>{
+            setRight(0);
+        }, 500); 
+    }, [header_text]); 
+
+
+
+return <div id='contactpage' style={{right:pageright}}>
+    <Header text={header_text} fontSize={24} />
+    <CustomButton text="email" onClick={()=>{
+       
+        setHeader(headers_arr[index]);
+        index++;
+        if(index > headers_arr.length){
+            index=0; 
+        }
+    }} />
 </div>
 }
 
